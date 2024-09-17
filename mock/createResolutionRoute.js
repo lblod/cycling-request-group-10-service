b428763a5ead63f6c75cd82d7f007f7e6b3693ea
Dennis Van Eecke <dennis.vaneecke@gmail.com>
2024-09-17 11:30:46 +0000

    Mock endpoint OK

--- mock/createResolutionRoute.js
+++ mock/createResolutionRoute.js
@@ -2,8 +2,9 @@ import { app } from 'mu';
 import { SPARQL_PREFIXES, PUBLIC_GRAPH } from '../env';
 import { v4 as uuid } from 'uuid';
 import dayjs from 'dayjs';
-import { dateToSparqlLiteral } from '../lib/util';
+import { dateToSparqlLiteral, toExtNode } from '../lib/util';
 import { querySudo as query } from '@lblod/mu-auth-sudo';
+import bodyParser from 'body-parser';
 
 /**
  * @typedef {Object} MockResolutionBody
@@ -16,7 +17,7 @@ import { querySudo as query } from '@lblod/mu-auth-sudo';
  * @param { Express.Request } req
  * @param { Express.Response} res
  */
-const creatMockResolution = async (req,res) => {
+const createMockResolution = async (req,res) => {
   /** @type {MockResolutionBody} */
   const body = req.body;
 
@@ -26,49 +27,46 @@ const creatMockResolution = async (req,res) => {
   const resolutionId = uuid();
   const publishedResourceId = uuid();
 
-  const bvapNode = `ext:${bvapId()}`
-  const resolutionNode = `ext:${resolutionId()}`
-  const publishedResourceNode = `ext:${publishedResourceId()}`
-
   const resolutionTitle = `Mock resolution for cycling competition in ${body.municipalityName}`
 
-  const resolutionDescription = ```
+  const resolutionDescription = `
     Mock resolution for ${body.municipalityName}:
     This resolution was generated using a mock endpoint. It will trigger a delta message and this will further the cycling application process automatically."
-  ```
+  `
   
-  const sparql = ```
+  const sparql = `
   ${SPARQL_PREFIXES}
 
   INSERT DATA {
     GRAPH <${PUBLIC_GRAPH}> {
-      ext:resolution/${resolutionId} a besluit:Besluit;
+      ${toExtNode('besluit',resolutionId)} a besluit:Besluit;
         mu:uuid "${resolutionId}";
         eli:description ${JSON.stringify(resolutionDescription)};
         eli:title_short "${resolutionTitle}";
         besluit:motivering ${JSON.stringify(resolutionDescription)};
-        eli:date_publication $${dateToSparqlLiteral(now)};
+        eli:date_publication ${dateToSparqlLiteral(now)};
         prov:value ${JSON.stringify(resolutionDescription)};
         eli:language "nl";
         eli:title "${resolutionTitle}";
         nao:score 1;
-        prov:generated ext:bvap/${bvapId}.
-      ext:resolution/${bvapId} a besluit:BehandelingVanAgendapunt;
+        prov:generated ${toExtNode('bvap',bvapId)} .
+      ${toExtNode('bvap',bvapId)} a besluit:BehandelingVanAgendapunt;
         dct:subject <${body.agendaItemUri}>.
-      ext:published-resources/${publishedResourceId} a sign:PublishedResource;
-        sign:text ${resolutionDescription}.
+      ${toExtNode('published-resources',publishedResourceId)} a sign:PublishedResource;
+        sign:text ${JSON.stringify(resolutionDescription)}.
     }
   }
-  ```;
+  `;
   try {
     await query(sparql);
   } catch (e) {
-    res.status(500).json({message: e.message, sparql});
+    res.status(500).json({message: e.message, sparql,error:e});
+    return;
   }
   res.status(200).json({success:true});
 
 }
 
-export function installAgendaItemRoutes(app) {
-  app.post('/mock/resolution', creatMockResolution);
+export function installMockRoutes(app) {
+  app.post('/mock/resolution',  bodyParser.json({ limit: '50mb' }), createMockResolution);
 }
